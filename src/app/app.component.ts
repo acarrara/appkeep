7be5b091ad2ffe1e5f46d428f81172f2e1059f2a
Alessandro Carrara <carrara@esteco.com>
2019-10-21 08:17:32 +0200

    WIP: removed color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppKeep } from './models/AppKeep';
@@ -7,19 +7,19 @@ import { AppKeep } from './models/AppKeep';
   selector: 'ak-app',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  title = 'appkeep';
   appKeeps$: Observable<AppKeep[]>;
-  hues: number[] = [90, 240, 300];
-  shades: number[] = [48, 54, 60, 66, 72, 78, 84, 90, 96];
   value = 0;
+
+  @ViewChild('amount', {static: true})
+  amount: ElementRef;
 
   constructor(private http: HttpClient) {
     this.appKeeps$ = this.http.get<AppKeep[]>('/api/appkeeps');
   }
 
-  format(appKeep: AppKeep) {
-    return `[${appKeep.type}] ${appKeep.title} (${new Date(appKeep.date)}) - ${appKeep.amount}`;
+  ngAfterViewInit(): void {
+    this.amount.nativeElement.focus();
   }
 }

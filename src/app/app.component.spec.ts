dfa193b03a3c067988a702cfcc740e33a316d142
Alessandro Carrara <carrara@esteco.com>
2019-11-04 19:22:07 +0100

    test(app): fix broken tests

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  it('should create the app', () => {
+    const app = new AppComponent();
+    expect(app).toBeTruthy();
+  });
+});

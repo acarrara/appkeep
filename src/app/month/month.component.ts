280680af80ad3236e304fb0d1ab94c9dbdec12e1
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-12-10 23:48:07 +0100

    feat(month): add static month page attempt

---
+++ src/app/month/month.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'ak-month',
+  templateUrl: 'month.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MonthComponent {
+}

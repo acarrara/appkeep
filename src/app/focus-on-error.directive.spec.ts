9315f509fc65ec949519e61e836d770adc7e22a5
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-12-17 21:41:13 +0100

    fix(inputs): fix broken test

--- src/app/focus-on-error.directive.spec.ts
+++ src/app/focus-on-error.directive.spec.ts
@@ -2,7 +2,7 @@ import { FocusOnErrorDirective } from './focus-on-error.directive';
 
 describe('FocusOnErrorDirective', () => {
   it('should create an instance', () => {
-    const directive = new FocusOnErrorDirective();
+    const directive = new FocusOnErrorDirective(null);
     expect(directive).toBeTruthy();
   });
 });

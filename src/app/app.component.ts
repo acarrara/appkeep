b8e4fcdc9dab0d50cfa7528c4d107d3cd93cafaf
Alessandro Carrara <alessandro.carrara@gmail.com>
2019-12-18 19:23:19 +0100

    style(appkeep): cleanup patterns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiAuthenticationService } from './api-authentication.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ak-app',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  loading$: Observable<boolean>;
+
+  constructor(auth: ApiAuthenticationService) {
+    this.loading$ = auth.loggedIn$.pipe(map(loggedIn => !loggedIn));
+  }
 }

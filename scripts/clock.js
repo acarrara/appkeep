6274fb475c48009a6eea48cf9a4dc94699d76eef
Alessandro Carrara <alessandro.carrara@gmail.com>
2020-05-20 14:11:54 +0200

    fix(monthly-appkeeps): fix cron task

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,11 +1,9 @@
 const CronJob = require('cron').CronJob;
 const storeMonthlyAppKeeps = require('./storeMonthlyAppKeeps.js');
 
-const job = new CronJob({
-  cronTime: '* * * * *',
-  onTick: storeMonthlyAppKeeps.sayHello(),
+new CronJob({
+  cronTime: '*/10 * * * * *',
+  onTick: () => storeMonthlyAppKeeps.sayHello(),
   start: true,
   timeZone: 'Europe/Rome'
 });
-
-job.start();

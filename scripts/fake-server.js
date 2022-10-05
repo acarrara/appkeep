bffbd5488b2bdb0a8c0751f9bd88eaa602749120
acarrara <alessandro.carrara@gmail.com>
2022-10-05 15:15:31 +0200

    fake server script

---
+++ scripts/fake-server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const enforce = require('express-sslify');
+const compression = require('compression');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(compression());
+
+if (process.env.production) {
+  app.use(enforce.HTTPS({trustProtoHeader: true}));
+}
+
+app.use(express.static(__dirname + '/../dist/appkeep'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../dist/appkeep/index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
